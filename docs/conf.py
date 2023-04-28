cfd54c93b4577921ef93f9389ad0eb72bb41f620
Pamphile Roy <roy.pamphile@gmail.com>
2023-04-28 13:31:39 +0200

    Use theme for docs and some settings

--- docs/conf.py
+++ docs/conf.py
@@ -14,14 +14,31 @@ release = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "numpydoc",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# -- autosummary -------------------------------------------------------------
+
+autosummary_generate = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "github_url": "https://github.com/Simulation-Decomposition/simdec-python",
+    "navbar_end": ["theme-switcher", "version-switcher", "navbar-icon-links"],
+    "switcher": {
+        "json_url": "https://simdec.readthedocs.io/en/latest/_static/version_switcher.json",  # noqa
+        "version_match": release,
+    },
+}
