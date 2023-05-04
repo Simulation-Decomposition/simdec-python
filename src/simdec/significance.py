15e1ea9c5b9716983259488bf698347a77ea9cd3
Pamphile Roy <roy.pamphile@gmail.com>
2023-05-04 22:41:44 +0200

    Partial soe

--- src/simdec/significance.py
+++ src/simdec/significance.py
@@ -25,6 +25,12 @@ def number_of_bins(n_runs: int, n_factors: int) -> tuple[int, int]:
     return n_bins_foe, n_bins_soe
 
 
+def _weighted_var(x: np.ndarray, weights: np.ndarray) -> np.ndarray:
+    avg = np.average(x, weights=weights)
+    variance = np.average((x - avg) ** 2, weights=weights)
+    return np.sqrt(variance)
+
+
 def significance(
     inputs: np.ndarray, output: np.ndarray, method: Literal["simdec"]
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -57,20 +63,37 @@ def significance(
     n_bins_foe, n_bins_soe = number_of_bins(n_runs, n_factors)
 
     # Overall variance of the output
-    # var_y = np.var(output)
+    var_y = np.var(output)
 
     si = np.empty(n_factors)
     foe = np.empty(n_factors)
-    soe = np.empty(n_factors)
+    soe = np.empty((n_factors, n_factors))
 
+    # first order
     for i in range(n_factors):
         xi = inputs[:, i]
 
-        bin_avg, _, bin_count = stats.binned_statistic(xi, output, bins=n_bins_foe)
+        bin_avg, _, bin_count = stats.binned_statistic(
+            x=xi, values=output, bins=n_bins_foe
+        )
 
         # weighted variance and divide by the overall variance of the output
-        avg = np.average(bin_avg, weights=bin_count)
-        variance = np.average((bin_avg - avg) ** 2, weights=bin_count)
-        foe[i] = np.sqrt(variance)
+        foe[i] = _weighted_var(bin_avg, weights=bin_count)
+
+        # second order
+        for j in range(n_factors):
+            if i == j and j < i:
+                continue
+
+            xj = inputs[:, i]
+            bin_avg, *_, bin_counts = stats.binned_statistic_2d(
+                x=xi, y=xj, values=output, bins=n_bins_soe, expand_binnumbers=True
+            )
+
+            var_ij = ...  # _weighted_var(bin_avg, weights=bin_counts)
+            var_i = _weighted_var(bin_avg, weights=bin_counts[0])
+            var_j = _weighted_var(bin_avg, weights=bin_counts[1])
+
+            soe[i, j] = (var_ij - var_i - var_j) / var_y
 
     return si, foe, soe
