9de4c26a353bb22e55e5a3c236f70a4eae8ba48e
Pamphile Roy <roy.pamphile@gmail.com>
2023-06-28 00:08:24 +0200

    Add typer and POC of CLI

--- src/simdec/workflow.py
+++ src/simdec/workflow.py
@@ -1,10 +1,30 @@
-import matplotlib.pyplot as plt
+from pathlib import Path
+from typing_extensions import Annotated
+
+
 import pandas as pd
+import typer
+
 import simdec as sd
 
+app = typer.Typer()
 
-def main():
-    data = pd.read_csv("../../tests/data/stress.csv", sep=";", decimal=",")
+
+@app.command()
+def main(
+    data: Annotated[
+        Path,
+        typer.Option(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ]
+):
+    data = pd.read_csv(data, sep=";", decimal=",")
     output_name, *v_names = list(data.columns)
     inputs, output = data[v_names], data[output_name]
 
@@ -14,14 +34,13 @@ def main():
     res = sd.decomposition(inputs=inputs, output=output, significance=si)
 
     ax, palette = sd.visualization(bins=res.bins, states=res.states)
-    plt.show()
 
     # use a notebook to see the styling
     table, styler = sd.tableau(
-        var_names=res.var_names, states=res.states, bins=res.bins, palette=palette
+        statistic=res.statistic,
+        var_names=res.var_names,
+        states=res.states,
+        bins=res.bins,
+        palette=palette,
     )
-    print(table, res.var_names, res.states)
-
-
-if __name__ == "__main__":
-    main()
+    print(styler.to_html())
