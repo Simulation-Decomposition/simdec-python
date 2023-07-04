249d59a93bbd7b82fb123dead0e871dff70b1213
Pamphile Roy <roy.pamphile@gmail.com>
2023-07-05 00:34:51 +0200

    Adjust version switcher

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import importlib.metadata
 
 project = "SimDec"
 copyright = "2023, SimDec Developers"
 author = "Pamphile Roy"
-release = "1.0.0"
+release = importlib.metadata.version("simdec")
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -29,7 +22,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 autosummary_generate = True
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
@@ -41,8 +33,8 @@ html_theme_options = {
     "pygment_light_style": "github-light-colorblind",
     "pygment_dark_style": "pitaya-smoothie",
     "github_url": "https://github.com/Simulation-Decomposition/simdec-python",
-    # "switcher": {
-    #     "json_url": "https://simdec.readthedocs.io/en/latest/_static/version_switcher.json",  # noqa
-    #     "version_match": release,
-    # },
+    "switcher": {
+        "json_url": "https://simdec.readthedocs.io/en/latest/_static/version_switcher.json",  # noqa
+        "version_match": release,
+    },
 }
